5b5d27c52c1236514d8c0114f69e4bcf1f1f781f
AkshatGKMit <akshat@gkmit.co>
2024-10-10 18:00:44 +0530

    fix: Formatting issues

--- App.tsx
+++ App.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { SafeAreaView, Text } from "react-native";
 
 const App = () => {
-	return (
-		<SafeAreaView>
-			<Text>Event Manager</Text>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView>
+      <Text>Event Manager</Text>
+    </SafeAreaView>
+  );
 };
 
 export default App;
