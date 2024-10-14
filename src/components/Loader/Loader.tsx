6cfc38ad2f4b9e900ad410c4c0a25c1627714685
AkshatGKMit <akshat@gkmit.co>
2024-10-14 11:05:58 +0530

    feat: Add radio and text button

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import LoaderKit from "react-native-loader-kit";
 
-const Loader = ({ animName, color, size }: LoaderPorops) => {
-	return (
-		<LoaderKit
-			style={{ width: size, height: size }}
-			name={animName}
-			color={color}
-		/>
-	);
+const Loader = ({ animName, color, size }: LoaderProps) => {
+  return (
+    <LoaderKit
+      style={{ width: size, height: size }}
+      name={animName}
+      color={color}
+    />
+  );
 };
 
 export default Loader;
