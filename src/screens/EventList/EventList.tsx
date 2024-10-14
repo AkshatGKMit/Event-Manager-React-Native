be41246bf7f95424cb47143c14c3bcf55bb15443
AkshatGKMit <akshat@gkmit.co>
2024-10-14 17:29:16 +0530

    Add floating button

--- src/screens/EventList/EventList.tsx
+++ src/screens/EventList/EventList.tsx
@@ -1,10 +1,15 @@
 import { View, Text } from "react-native";
 import styles from "./styles";
+import IconButton from "@src/components/IconButton";
 
 const EventList = () => {
   return (
     <View style={styles.screen}>
       <Text style={styles.text}>EventList</Text>
+      <IconButton
+        icon={{ family: "Ionicons", name: "add" }}
+        onPress={() => {}}
+      />
     </View>
   );
 };
